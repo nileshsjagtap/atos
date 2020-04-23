9df40af9f84da33bc2e16b5782fd71fc7d55e68c
Nilesh Jagtap <nilesh.jagtap@springernature.com>
2020-04-23 12:32:03 +0530

    added abort condition

--- src/main/scala/assignment2/Run.scala
+++ src/main/scala/assignment2/Run.scala
@@ -4,7 +4,7 @@ import assignment1.FileWriter
 
 object Run extends App {
 
-  for {
+  val bill = for {
     canvas <- Canvas(1000, 1000)
     rectangle <- Rectangle(10, 10, 30, 40, canvas)
     square <- Square(15, 30, 35, canvas)
@@ -13,7 +13,12 @@ object Run extends App {
     textBox <- TextBox(5, 5, 200, 100, canvas, "sample text")
     bill <- MaterialBill[BuilderSystem.type](List(rectangle, square, ellipse, circle, textBox))
   } yield {
-    FileWriter.write(bill,"src/main/scala/assignment2/output")
+    bill
+  }
+
+  bill match {
+    case Right(bill) => FileWriter.write(bill,"src/main/scala/assignment2/output")
+    case Left(err) => FileWriter.write(err.toString+"\n"+"+++++Abort+++++","src/main/scala/assignment2/output")
   }
 
 }
